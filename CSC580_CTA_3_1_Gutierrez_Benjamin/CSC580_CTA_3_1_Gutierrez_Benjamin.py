aea7780c51fac0da7acebe3d265fa306bbd536f1
Benjamin Gutierrez <ben.gutierrez@simspace.com>
2023-01-29 22:02:16 -0500

    saving work

--- CSC580_CTA_3_1_Gutierrez_Benjamin/CSC580_CTA_3_1_Gutierrez_Benjamin.py
+++ CSC580_CTA_3_1_Gutierrez_Benjamin/CSC580_CTA_3_1_Gutierrez_Benjamin.py
@@ -6,22 +6,126 @@ Instructor: Lori Farr
 Assignment: Module 3 Critical Thinking Assignment Option 1
 """
 
-# Load in the Tox21 Dataset
 import numpy as np
 import tensorflow.compat.v1 as tf
 import matplotlib.pyplot as plt
-import deepchem as dc
-from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
-    # Set seed value to generate reproducible results
-    seed_value = 456
-    np.random.seed(456)
-    tf.set_random_seed(456)
+    tf.disable_v2_behavior()
+    tf.compat.v1.disable_eager_execution()
 
-    # Get training & testing data
-    _, (train, valid, test), _ = dc.molnet.load_tox21()
-    train_X, train_y, train_w = train.X, train.y, train.w
-    valid_X, valid_y, valid_w = valid.X, valid.y, valid.w
-    test_X, test_y, test_w = test.X, test.y, test.w
+    # Make random numbers predictable
+    seed_value = 101
+    np.random.seed(seed_value)
+    tf.set_random_seed(seed_value)
 
+    # Generate some random data for training the linear regression model
+    # There will be 50 data points ranging from 0 to 50
+    num_points = 50
+    x = np.linspace(0, 50, num_points)
+    y = np.linspace(0, 50, num_points)
+
+    # Adding noise to the random linear data
+    x += np.random.uniform(-4, 4, num_points)
+    y += np.random.uniform(-4, 4, num_points)
+    n = len(x)  # Number of data points
+
+    # 1) plot the training data
+    plt.scatter(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title("Training Data")
+    # save the plot
+    plt.savefig("training_data.png")
+    plt.show()
+
+
+    # 2) Create a TensorFlow model by defining the placeholders X and Y
+    X = tf.placeholder("float")
+    Y = tf.placeholder("float")
+
+    # 3) Define the weights W and bias b
+    Weight = tf.Variable(np.random.randn(), name="W")
+    bias = tf.Variable(np.random.randn(), name="b")
+
+    # 4) Define hyperparameters for the model
+    learning_rate = 0.01
+    training_epochs = 1000
+
+    # 5) Define the hypothesis
+    hypothesis = X * Weight + bias
+
+    # Define the cost function
+    cost = tf.reduce_sum(tf.pow(hypothesis - Y, 2)) / (2 * n)
+
+    # Define the optimizer
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+    # 6) Implement the training process inside a TensorFlow session
+    session = tf.InteractiveSession()
+
+    # Initialize the variables W and b
+    session.run(tf.global_variables_initializer())
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (_x, _y) in zip(x, y):
+            session.run(optimizer, feed_dict={X: _x, Y: _y})
+
+        # Display logs per epoch step
+        if (epoch + 1) % 50 == 0:
+            # Calculate the cost
+            c = session.run(cost, feed_dict={X: x, Y: y})
+            print(f"Epoch {epoch + 1}: cost = {c} Weight = {session.run(Weight)}, bias = {session.run(bias)}")
+
+    # 7) Print out the results for the training cost, weight, and bias
+    training_cost = session.run(cost, feed_dict={X: x, Y: y})
+    calculated_weight = session.run(Weight)
+    calculated_bias = session.run(bias)
+    print(f"Training cost = {training_cost} Weight = {calculated_weight} bias = {calculated_bias}")
+
+    # 8) Plot the training data and the linear regression model
+    plt.plot(x, y, 'ro', label='Original data')
+    calculated_y = calculated_weight * x + calculated_bias
+    plt.plot(x, calculated_y, label='Fitted line')
+    plt.title('Linear Regression Result')
+    plt.legend()
+    # save the plot
+    plt.savefig('linear_regression.png')
+    plt.show()
+
+
+ # n = len(x) # number of observations
+ #
+ #    # Configure model
+ #    learning_rate = 0.5
+ #    num_epochs = 2000
+ #
+ #    # Equation, cost, & optimizer
+ #    eqn = a * tf.pow(X, 2) + b * X + c
+ #    cost = tf.reduce_sum(tf.pow(eqn - Y, 2)) / (2 * n)
+ #    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+ #
+ #    with tf.Session() as sess:
+ #        sess.run(tf.global_variables_initializer())
+ #        for epoch in range(num_epochs):
+ #            for (_x, _y) in zip(x, y):
+ #                sess.run(optimizer, feed_dict={X: _x, Y: _y})
+ #            if (epoch + 1) % 200 == 0:
+ #                cost = sess.run(eqn, feed_dict={X: x, Y: y})
+ #                print("Epoch: ", epoch + 1, "Cost: ", cost, "a: ", sess.run(a), "b: ", sess.run(b), "c: ",
+ #                      sess.run(c))
+ #
+ #        coeff_a = sess.run(a)
+ #        coeff_b = sess.run(b)
+ #        coeff_c = sess.run(c)
+ #
+ #    # 8) Plot the training data and the polynomial regression model
+ #    plt.plot(x, y, 'ro', label='Original data')
+ #    calculated_y = coeff_a * np.power(x, 2) + coeff_b * x + coeff_c
+ #    plt.plot(x, calculated_y, label='Fitted polynomial')
+ #    plt.title('Polynomial Regression Result')
+ #    plt.legend()
+ #    # save the plot
+ #    plt.savefig('linear_regression.png')
+ #    plt.show()
--- CSC580_CTA_4_1_Gutierrez_Benjamin/CSC580_CTA_4_1_Gutierrez_Benjamin.py
+++ CSC580_CTA_4_1_Gutierrez_Benjamin/CSC580_CTA_4_1_Gutierrez_Benjamin.py
@@ -1,95 +1,145 @@
 """
 Student: Benjamin Gutierrez
-Date: January 7, 2023
+Date: January 29, 2023
 Course: Applying Machine Learning and Neural Networks - Capstone
 Instructor: Lori Farr
-Assignment: Module 3 Critical Thinking Assignment Option 1
+Assignment: Module 4 Critical Thinking Assignment Option 1
 """
+
+# Step 1: Load the Tox21 Dataset.
 
 import numpy as np
 import tensorflow.compat.v1 as tf
 import matplotlib.pyplot as plt
+import deepchem as dc
+from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
+    # setup tensorflow
     tf.disable_v2_behavior()
     tf.compat.v1.disable_eager_execution()
 
-    # Make random numbers predictable
-    seed_value = 101
-    np.random.seed(seed_value)
-    tf.set_random_seed(seed_value)
+    # set seed for reproducibility
+    seed = 456
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
 
-    # Generate some random data for training the linear regression model
-    # There will be 50 data points ranging from 0 to 50
-    num_points = 50
-    x = np.linspace(0, 50, num_points)
-    y = np.linspace(0, 50, num_points)
+    _, (train, valid, test), _ = dc.molnet.load_tox21()
+    train_X, train_y, train_w = train.X, train.y, train.w
+    valid_X, valid_y, valid_w = valid.X, valid.y, valid.w
+    test_X, test_y, test_w = test.X, test.y, test.w
 
-    # Adding noise to the random linear data
-    x += np.random.uniform(-4, 4, num_points)
-    y += np.random.uniform(-4, 4, num_points)
-    n = len(x)  # Number of data points
+    # Step 2: Remove extra datasets.
 
-    # 1) plot the training data
-    plt.scatter(x, y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title("Training Data")
-    # save the plot
-    plt.savefig("training_data.png")
-    plt.show()
+    # Remove extra tasks
+    train_y = train_y[:, 0]
+    valid_y = valid_y[:, 0]
+    test_y = test_y[:, 0]
+    train_w = train_w[:, 0]
+    valid_w = valid_w[:, 0]
+    test_w = test_w[:, 0]
+
+    # Step 3: Define placeholders that accept minibatches of different sizes.
+
+    # Generate tensorflow graph
+
+    d = 1024
+    n_hidden = 50
+    learning_rate = .001
+    n_epochs = 10
+    batch_size = 100
+
+    with tf.name_scope("placeholders"):
+        x = tf.placeholder(tf.float32, (None, d))
+
+        y = tf.placeholder(tf.float32, (None,))
+
+    # Step 4: Implement a hidden layer.
+
+    with tf.name_scope("hidden-layer"):
+        W = tf.Variable(tf.random_normal((d, n_hidden)))
+
+        b = tf.Variable(tf.random_normal((n_hidden,)))
+
+        x_hidden = tf.nn.relu(tf.matmul(x, W) + b)
+
+    # Step 5: Complete the fully connected architecture.
+
+    with tf.name_scope("output"):
+        W = tf.Variable(tf.random_normal((n_hidden, 1)))
+        b = tf.Variable(tf.random_normal((1,)))
+        y_logit = tf.matmul(x_hidden, W) + b
+        # the sigmoid gives the class probability of 1
+        y_one_prob = tf.sigmoid(y_logit)
+        # Rounding P(y=1) will give the correct prediction.
+        y_pred = tf.round(y_one_prob)
+
+    with tf.name_scope("loss"):
+        # Compute the cross-entropy term for each datapoint
+        y_expand = tf.expand_dims(y, 1)
+        entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=y_logit, labels=y_expand)
+        # Sum all contributions
+        l = tf.reduce_sum(entropy)
+
+    with tf.name_scope("optim"):
+        train_op = tf.train.AdamOptimizer(learning_rate).minimize(l)
+
+    with tf.name_scope("summaries"):
+        tf.summary.scalar("loss", l)
+        merged = tf.summary.merge_all()
+
+    # Step 6: Add dropout to a hidden layer.
+    with tf.name_scope("dropout"):
+        keep_prob = tf.placeholder(tf.float32)
+        x_hidden_drop = tf.nn.dropout(x_hidden, keep_prob)
+
+    # Step 7: Define a hidden layer with dropout.
+    with tf.name_scope("hidden-layer-dropout"):
+        W = tf.Variable(tf.random_normal((d, n_hidden)))
+        b = tf.Variable(tf.random_normal((n_hidden,)))
+        x_hidden_drop = tf.nn.relu(tf.matmul(x, W) + b)
+        x_hidden_drop = tf.nn.dropout(x_hidden_drop, keep_prob)
+
+    # Step 8: Implement mini - batching training.
+
+    train_writer = tf.summary.FileWriter('/tmp/fcnet-tox21',tf.get_default_graph())
+    N = train_X.shape[0]
+    epoch_losses = []
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        step = 0
+        for epoch in range(n_epochs):
+            pos = 0
+            pos_losses = []
+            while pos < N:
+                batch_X = train_X[pos:pos + batch_size]
+                batch_y = train_y[pos:pos + batch_size]
+                feed_dict = {x: batch_X, y: batch_y}
+                _, summary, loss = sess.run([train_op, merged, l], feed_dict=feed_dict)
+                pos_losses.append(loss)
+                print("epoch %d, step %d, loss: %f" % (epoch, step, loss))
+                train_writer.add_summary(summary, step)
+                step += 1
+                pos += batch_size
+            epoch_losses.append(np.mean(pos_losses))
+
+        # Make Predictions
+
+        valid_y_pred = sess.run(y_pred, feed_dict={x: valid_X})
+
+        # Step 9: Use TensorBoard to track model convergence
+        # use tensorboard --logdir=/tmp/fcnet-tox21 to view the graph
+
+        # print the model accuracy
+        print("Validation Accuracy: %f" % accuracy_score(valid_y, valid_y_pred))
+
+        # plot the loss curve
+        plt.plot(epoch_losses)
+        plt.title("Loss Curve")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.show()
 
 
-    # 2) Create a TensorFlow model by defining the placeholders X and Y
-    X = tf.placeholder("float")
-    Y = tf.placeholder("float")
 
-    # 3) Define the weights W and bias b
-    Weight = tf.Variable(np.random.randn(), name="W")
-    bias = tf.Variable(np.random.randn(), name="b")
 
-    # 4) Define hyperparameters for the model
-    learning_rate = 0.01
-    training_epochs = 1000
-
-    # 5) Define the hypothesis
-    hypothesis = X * Weight + bias
-
-    # Define the cost function
-    cost = tf.reduce_sum(tf.pow(hypothesis - Y, 2)) / (2 * n)
-
-    # Define the optimizer
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-    # 6) Implement the training process inside a TensorFlow session
-    session = tf.InteractiveSession()
-
-    # Initialize the variables W and b
-    session.run(tf.global_variables_initializer())
-
-    # Fit all training data
-    for epoch in range(training_epochs):
-        for (_x, _y) in zip(x, y):
-            session.run(optimizer, feed_dict={X: _x, Y: _y})
-
-        # Display logs per epoch step
-        if (epoch + 1) % 50 == 0:
-            # Calculate the cost
-            c = session.run(cost, feed_dict={X: x, Y: y})
-            print(f"Epoch {epoch + 1}: cost = {c} Weight = {session.run(Weight)}, bias = {session.run(bias)}")
-
-    # 7) Print out the results for the training cost, weight, and bias
-    training_cost = session.run(cost, feed_dict={X: x, Y: y})
-    calculated_weight = session.run(Weight)
-    calculated_bias = session.run(bias)
-    print(f"Training cost = {training_cost} Weight = {calculated_weight} bias = {calculated_bias}")
-
-    # 8) Plot the training data and the linear regression model
-    plt.plot(x, y, 'ro', label='Original data')
-    calculated_y = calculated_weight * x + calculated_bias
-    plt.plot(x, calculated_y, label='Fitted line')
-    plt.title('Linear Regression Result')
-    plt.legend()
-    # save the plot
-    plt.savefig('linear_regression.png')
-    plt.show()
