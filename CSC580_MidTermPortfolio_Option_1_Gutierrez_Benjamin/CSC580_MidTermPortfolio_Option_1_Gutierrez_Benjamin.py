836290419650ea1974eacd783f265e37abf55eab
Benjamin Gutierrez <ben.gutierrez@simspace.com>
2023-01-29 23:01:41 -0500

    windows

--- CSC580_MidTermPortfolio_Option_1_Gutierrez_Benjamin/CSC580_MidTermPortfolio_Option_1_Gutierrez_Benjamin.py
+++ CSC580_MidTermPortfolio_Option_1_Gutierrez_Benjamin/CSC580_MidTermPortfolio_Option_1_Gutierrez_Benjamin.py
@@ -1,9 +1,35 @@
 import face_recognition
-known_image = face_recognition.load_image_file("individual_face_in_photo.jpg")
-unknown_image = face_recognition.load_image_file("group_faces.jpg")
 
-biden_encoding = face_recognition.face_encodings(known_image)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+def is_individual_in_photo(individual_filepath: str, group_filepath: str) -> bool:
+    """ Tells whether an individual is in a group photo or not.
+        Parameters:
+            individual_filepath: filepath of the individual to check
+            group_filepath: filepath of a group photo the individual may be in
+        Returns:
+            True if the indvidual is in the photo, otherwise false
+    """
 
-results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-print(results)
+    known_image = face_recognition.load_image_file("individual_face_in_photo.jpg")
+    unknown_image = face_recognition.load_image_file("group_faces.jpg")
+
+    known_encoding = face_recognition.face_encodings(known_image)[0]
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+    results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+    if True in results:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    in_photo_filepath = "individual_face_in_photo.jpg"
+    not_in_photo_filepath = "individual_face_not_in_photo.jpg"
+    group_filepath = "group_faces.jpg"
+
+    in_photo_result = is_individual_in_photo(in_photo_filepath, group_filepath)
+    print(f"Is {in_photo_filepath} contained in {group_filepath}?")
+    print(in_photo_result)
+
+    not_in_photo_result = is_individual_in_photo(not_in_photo_filepath, group_filepath)
+    print(f"Is {not_in_photo_filepath} contained in {group_filepath}?")
+    print(in_photo_result)
